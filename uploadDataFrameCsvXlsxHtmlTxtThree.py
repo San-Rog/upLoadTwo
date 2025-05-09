2b79f8216fdcd7ce7591c66be0d1a4370e041da6
San-Rog <118987989+San-Rog@users.noreply.github.com>
2025-05-09 00:39:13 -0300

    Update uploadDataFrameCsvXlsxHtmlTxtThree.py

--- uploadDataFrameCsvXlsxHtmlTxtThree.py
+++ uploadDataFrameCsvXlsxHtmlTxtThree.py
@@ -230,9 +230,9 @@ def main():
     dateCurrUse = {key:[] for key in keyCurrent}
     months = {1: 'janeiro', 2: 'fevereiro', 3: 'março', 4: 'abril', 5:'maio', 6: 'junho', 
               7: 'julho', 8: 'agosto', 9: 'setembro', 10: 'outubro', 11: 'novembro', 12: 'dezembro'}
-    weeks = {0: 'domingo', 1: 'segunda-feira', 2: 'terça-feira', 
-             3: 'quarta-feira', 4: 'quinta-feira', 5: 'sexta-feira', 
-             6: 'sábado'}
+    weeks = {6: 'domingo', 0: 'segunda-feira', 1: 'terça-feira', 
+             2: 'quarta-feira', 3: 'quinta-feira', 4: 'sexta-feira', 
+             5: 'sábado'}
     dateNow = datetime.date.today()
     d = date(2025, 5, 9)
     #args = [(d, 12, 0, 'Contagem em dias corridos', 'Demonstrativo 1'), 
